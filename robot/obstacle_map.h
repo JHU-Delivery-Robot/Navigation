72bbeabe1cdbc190866a4614d67d98ac8bbf0fb2
Rishibrata Biswas <rbis1001@gmail.com>
2020-11-22 20:06:20 -0500

    Obstacle Map: Initial implementation
    Updated comments, removed some typedefs, Changed makefile flags to be
    C11 and include math library.

--- robot/obstacle_map.h
+++ robot/obstacle_map.h
@@ -11,27 +11,28 @@
  * things like function names and types to things that make more sense.
  */
 
+#ifndef OBSTACLE_MAP_H
+#define OBSTACLE_MAP_H
+
 #include <stddef.h>
 #include <stdint.h>
+#include "stb_image.h"
 
 /* ----------------------------------------------------------------------
  *   Defines, typedefs
  * ---------------------------------------------------------------------- */
 
-/* Physical length of pixel */
-#define PIXEL_PHYSICAL_LENGTH 2   /* Two centimeters? */
-
-typedef unint8_t Obst_Map_Px;     /* Single pixel in obstacle map */
-typedef uint32_t Pixel_Dimen;
+typedef uint8_t Obst_Map_Px;     /* Single pixel in obstacle map */
+typedef unsigned Pixel_Dimen;
 /* A length in our physical world with real units (could be meters, 
  * cm, etc --- we need to decide).  Do we want to use a double for this? */
-typedef uint32_t Physical_Dimen;
+typedef unsigned Physical_Dimen;
 
 /* Structure for storing obstacle map and relevant information */
 typedef struct {
-  Obst_Map_Px *map;             /* bitmap data */
-  Pixel_Dimen width;            /* width of bitmap array (map width in pixels) */
-  Pixel_Dimen height;           /* height of bitmap array (map height in pixels) */
+	Obst_Map_Px *map;     /* bitmap data */
+	Pixel_Dimen width;            /* width of bitmap array (map width in pixels) */
+	Physical_Dimen height;           /* height of bitmap array (map height in pixels) */
 } Obst_Map;
 
 
@@ -50,6 +51,15 @@ typedef struct {
 Obst_Map *load_obstacle_map(const char *pathname);
 
 /*
+ * Frees a heap-allocated Obst_Map struct
+ * Args: 
+ *   omap - pointer heap-allocated to Obst_Map struct
+ * Returns:
+ *   N/A
+ */
+void free_obst_map(Obst_Map *omap);
+
+/*
  * Computes distance to nearest obstacle from a given point in a given 
  * direction.
  * Args: 
@@ -59,8 +69,6 @@ Obst_Map *load_obstacle_map(const char *pathname);
  * Returns: 
  *   double float (for some semblance of precision) of distance 
  *     in pixel units from given point to nearest obstacle
- *
- * 
  */
 double dist_to_obstacle(Pixel_Dimen x, Pixel_Dimen y, double angle);
 
@@ -77,7 +85,6 @@ double dist_to_obstacle(Pixel_Dimen x, Pixel_Dimen y, double angle);
  *     in pixel units from given point to nearest obstacle IF
  *     obstacle was found within limit.  OTHERWISE, returns -1.0d0 
  *     to indicate that no obstacle was found within limit.
- *
  */
 double dist_to_obstacle_limited(Pixel_Dimen x, Pixel_Dimen y, double angle, Pixel_Dimen limit);
 
@@ -86,3 +93,5 @@ double dist_to_obstacle_limited(Pixel_Dimen x, Pixel_Dimen y, double angle, Pixe
  * lengths and vice versa.  Should we inline these? */
 Physical_Dimen pixel_to_phys_dimen(Pixel_Dimen length);
 Pixel_Dimen phys_to_pixel_dimen(Physical_Dimen length);
+
+#endif
