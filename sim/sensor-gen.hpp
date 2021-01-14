9df65d6da75000a08e159d98e790e924b1f1cd9d
Max Hahn <mhahn12@jhu.edu>
2021-01-14 13:25:47 -0600

    Sim: Refactor Sensor Generation API
    Refactored to use better object hierarchy.

--- sim/sensor-gen.hpp
+++ sim/sensor-gen.hpp
@@ -1,13 +1,14 @@
-/*
- * JHU Deliverbot Navigation Group 
- * Navigation simulation system 
- * Generate sensor data for simulation --- header file 
+/**
+ * @file sensor-gen.hpp
+ * JHU Deliverbot Navigation Group
+ * Navigation simulation system
+ * Generate sensor data for simulation --- header file
  *
- * @avnoks (GitHub)  5 Jan 2021 (create date)
- *
+ * @author avnoks (GitHub)  5 Jan 2021 (create date)
+ * @author <insert implementer username here>
  */
 
-/* A NOTICE (5 Jan 2020): We're at an early stage.  Feel free to change 
+/* A NOTICE (5 Jan 2020): We're at an early stage.  Feel free to change
  * things like function names and types to things that make more sense.
  */
 
@@ -15,86 +16,110 @@
 
 #define SENSOR_GEN
 /* Need to include obstacle map header file */ //#include ""
-
+// forward declare obstaclemap type
+class Obst_Map;
+// typedefs
 typedef unsigned Length;
 typedef unsigned Angle;
 
+/**
+ * Class that defines a beammodel
+ *
+ * Note that the trivial copy construuctor/assignment operator is used which
+ * will assign fields element by element. If additional pointer are added as
+ * fields, this will need to be overridden
+ */
 class BeamModel {
-public:
-	double std_dev;
-	double lambda;
-	double max_reading;
-
-	double w_exp;
-	double w_hit;
-	double w_max;
-	double w_rand;
-
-	/* 
-	 * Sample sensor reading from distribution described by statistics.
-	 * Arguments:
-	 *   dist   -  true distance to obstacle
-	 * Returns: 
-	 *   double float sampled from distribution
-	 */
-	Length sample(Length dist);
-};
-
-class Lidar {
 private:
-	BeamModel beam;
-
+    double std_dev;     /**< std deviation of the gaussian sensor dist. (accuracy) */
+    double lambda;      /**< parameter for an exponential dist. of short reading */
+    double max_reading; /**< recip. prob for the unifgorm dist. used for glitches. This
+                             is also the maximum possible reading */
+    // probability weight of each of the 4 cases
+    double w_exp;  /**< weight of the exponential component */
+    double w_hit;  /**< weight of the gaussiawn component */
+    double w_max;  /**< weight of the probability of gatting a max value reading*/
+    double w_rand; /**< weight of the uniform distribution */
 public:
-	/* 
-	 * Generate Lidar readings and write to array. (Use BeamModel in lieu of sensor statistics)
-	 * Arguments
-	 *   readings - pointer to array to write Lidar readings to
-	 *   map      - obstacle map reference
-	 *   x        - x coordinate of lidar in obstacle map
-	 *   y        - y coordinate of lidar in obstacle map
-	 * Returns squat
-	 */
-	void generate(Length *readings, Obst_Map& map, Length x, Length y);
+    /**
+     * Constructor to create a new BeamModel
+     *
+     * @param map obstacle map to use to intialize values
+     * @param std_dev standard deviation fo sensor gaussian
+     * @param lambda parameter for exponential distribution
+     * @param max_reading maximum reading of sensor
+     * @param w_exp weight of exponential component
+     * @param w_rand wight of the uniform component
+     */
+    BeamModel(const Obst_Map& map, double std_dev, double lambda, double max_reading,
+              double w_exp, double w_rand);
+    /*
+     * Sample sensor reading from distribution described by statistics.
+     * @param dist true distance to obstacle
+     * @return float sampled from distribution
+     */
+    Length sample(Length dist);
 };
 
-class UltrasoundSensor {
+/**
+ * baseclass for all sensor data generation functions
+ */
+class SensorGen {
 private:
-	BeamModel beam;
-
+    BeamModel beam; /**< internal beammodel used by the sensor */
+    Obst_Map& map;  /**< obstacle map referenced by the sensor */
 public:
-	/* 
-	 * Generate ultrasound reading. (Use BeamModel in lieu of sensor statistics)
-	 * Arguments
-	 *   readings - pointer to array to write Lidar readings to
-	 *   map      - obstacle map reference
-	 *   x        - x coordinate of sensor in obstacle map
-	 *   y        - y coordinate of sensor in obstacle map
-	 *   heading  - angle in which the sensor is pointed
-	 */
-	Length generate(Length *readings, Obst_Map& map, Length x, Length y);
+    SensorGen(Obst_Map& map);
+    SensorGen(Obst_Map& map, BeamModel& mdl);
 };
 
-class IRSensor {
-private:
-	BeamModel beam;
-
+/**
+ * Lidar model
+ */
+class Lidar : public SensorGen {
 public:
-	/* 
-	 * Generate ultrasound reading. (Use BeamModel in lieu of sensor statistics)
-	 * Arguments
-	 *   map      - obstacle map reference
-	 *   x        - x coordinate of sensor in obstacle map
-	 *   y        - y coordinate of sensor in obstacle map
-	 *   heading  - angle in which the sensor is pointed
-	 */
-	Length IR_sensor(Obst_Map& map, Length x, Length y, Angle heading);
+    Lidar(Obst_Map& map, BeamModel& mdl);
+    /**
+     * Generate Lidar readings and write to array. (Use BeamModel in lieu of sensor statistics)
+     * @param readings pointer to array to write Lidar readings to
+     * @param x x coordinate of lidar in obstacle map
+     * @param y y coordinate of lidar in obstacle map
+     */
+    void generate(Length *readings, Length x, Length y);
 };
 
-//void lidar(Length *readings, Sensor_stats stats, Obst_Map map, Length x, Length y);
+/**
+ * Ultrasonic sensor model
+ */
+class UltrasoundSensor : public SensorGen {
+public:
+    UltrasoundSensor(Obst_Map& map, BeamModel& mdl);
+    /**
+     * Generate ultrasound reading. (Use BeamModel in lieu of sensor statistics)
+     *   @param x x coordinate of sensor in obstacle map
+     *   @param y y coordinate of sensor in obstacle map
+     *   @param fov size of ultrasonic "cone (can round to nearest half angle)
+     *   @param heading angle in which the sensor is pointed
+     *   @return simulated distance reading from sensor
+     */
+    Length generate(Length x, Length y, Angle fov, Angle heading);
+};
 
-//Length ultrasound_sensor(Sensor_stats stats, Obst_Map map, Length x, Length y, Angle heading);
-
-/* Implement this last.  :| */
-//Length IR_sensor(Sensor_stats stats, Length x, Length y, Angle heading);
+/**
+ * IR sensor model
+ */
+class IRSensor : public SensorGen {
+public:
+    IRSensor(Obst_Map& map, BeamModel& mdl);
+    /**
+     * Generate ultrasound reading. (Use BeamModel in lieu of sensor statistics)
+     *   @param map obstacle map reference
+     *   @param x x coordinate of sensor in obstacle map
+     *   @param y y coordinate of sensor in obstacle map
+     *   @param heading  - angle in which the sensor is pointed
+     *   @return simulated distance reading from sensor
+     */
+    Length IR_sensor(Length x, Length y, Angle heading);
+};
 
 #endif
