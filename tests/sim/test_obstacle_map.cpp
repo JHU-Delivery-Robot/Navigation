f76a870c29083d3df6b2b100375b67879889bfd7
Rishibrata Biswas <rbis1001@gmail.com>
2021-02-08 19:55:22 -0500

    Added obstacle test cases
    Tested Flat image loading and Unlimited distance to Obstacle
    From the testing, we learned that there is a strange y-offset by 1,
    and the detection algorithm is quite sensitive.
    
    Following this, we fixed the y-offset by referencing the top left
    of the image as the origin, and making y pointing downwards be
    considered positive. Test cases were then added for distance-
    limited, and all test cases now pass.

--- tests/sim/test_obstacle_map.cpp
+++ tests/sim/test_obstacle_map.cpp
@@ -1,6 +1,11 @@
+#include <string>
 #include "catch/catch.hpp"
 #include "obstacle_map.hpp"
 #include "stb/stb_image_write.h"
+
+#define COORD_TO_IN(c, r, n_c) (r * n_c + c)
+
+using std::string;
 
 /**
  * Test version of the obstacle map class
@@ -53,6 +58,143 @@ bool verify_match(ObstMapUUT map, char *img_data) {
         }
     }
     return true;
+}
+
+/**
+ * Verify that a map matches a flat image
+ * @param map obstable map to check
+ * @param img_data image to compare against
+ */
+bool verify_match_flat(ObstMapUUT map, char *img_data) {
+    unsigned char *img = reinterpret_cast<unsigned char*>(img_data);
+    std::vector<uint8_t> map_vals = map.getMap();
+    size_t img_size = map.getHeight()*map.getWidth();
+    for(size_t i = 0; i < img_size; i += 1) {
+        if(img[i] == 255 && map_vals[i] != 1) {
+            return false;
+        }
+        if(img[i] != 255 && map_vals[i] == 1) {
+            return false;
+        }
+    }
+    return true;
+}
+
+/**
+ * Verify that a map matches an input image
+ * @param map obstable map to check
+ * @param img_data image to compare against
+ */
+string generate_image() {
+    string test_map_img = "omap_distance_test.png";
+    size_t data_size = 600*600;
+    char *data = new char[data_size];
+
+    for (size_t i = 0; i < data_size; ++i) {
+        data[i] = static_cast<char>(0);
+    }
+
+    //Edge Case 1
+    data[COORD_TO_IN(0, 0, 600)] = static_cast<char>(255);
+    data[COORD_TO_IN(0, 1, 600)] = static_cast<char>(255);
+
+    //Edge Case 1
+    data[COORD_TO_IN(599, 0, 600)] = static_cast<char>(255);
+    data[COORD_TO_IN(599, 1, 600)] = static_cast<char>(255);
+
+    //Quadrant 1
+    data[COORD_TO_IN(379, 250, 600)] = static_cast<char>(255);
+    data[COORD_TO_IN(500, 200, 600)] = static_cast<char>(255);
+
+    //Quadrant 2
+    data[COORD_TO_IN(140, 150, 600)] = static_cast<char>(255);
+    data[COORD_TO_IN(200, 50, 600)] = static_cast<char>(255);
+
+    //Quadrant 3
+    data[COORD_TO_IN(50, 420, 600)] = static_cast<char>(255);
+    data[COORD_TO_IN(290, 500, 600)] = static_cast<char>(255);
+
+    //Quadrant 4
+    data[COORD_TO_IN(315, 435, 600)] = static_cast<char>(255);
+    data[COORD_TO_IN(550, 317, 600)] = static_cast<char>(255);
+
+    stbi_write_png(test_map_img.c_str(), 600, 600, 1, data, 600);
+
+    delete[] data;
+
+    return test_map_img;
+}
+
+TEST_CASE("Obstacle Map Distance Testing Unlimited",
+          "[ObstMap][ObstMap_Distance]") {
+    // some setup code
+    string test_map_img = generate_image();
+
+    ObstMapUUT uut(test_map_img.c_str());
+    REQUIRE(uut.getHeight() == 600);
+    REQUIRE(uut.getWidth() == 600);
+
+    //Edge Case 1
+    CHECK(uut.distToObstacle(300, 300, 2.356) == Approx(424.26406871).epsilon(0.001));
+
+    //Edge Case 2
+    CHECK(uut.distToObstacle(300, 300, 0.785) == Approx(422.849855).epsilon(0.001));
+
+    //Quadrant 1
+    CHECK(uut.distToObstacle(300, 300, 0.56) == Approx(93.49331526).epsilon(0.001));
+    CHECK(uut.distToObstacle(300, 300, 0.465) == Approx(223.60679774).epsilon(0.001));
+    
+    //Quadrant 2
+    CHECK(uut.distToObstacle(300, 300, 2.39) == Approx(219.31712199).epsilon(0.001));
+    CHECK(uut.distToObstacle(300, 300, 1.95) == Approx(269.25824035).epsilon(0.001));
+    
+    //Quadrant 3
+    CHECK(uut.distToObstacle(300, 300, 3.59) == Approx(277.30849247).epsilon(0.001));
+    CHECK(uut.distToObstacle(300, 300, 4.66) == Approx(200.24984395).epsilon(0.001));
+    
+    //Quadrant 4
+    CHECK(uut.distToObstacle(300, 300, 4.82) == Approx(135.83077707).epsilon(0.001));
+    CHECK(uut.distToObstacle(300, 300, 6.215) == Approx(250.57733337).epsilon(0.001));
+
+    //No pixel set here
+    CHECK(uut.distToObstacle(300, 300, 5.00) == -4.0);
+    CHECK(uut.distToObstacle(300, 300, 1.00) == -4.0);
+    
+    remove(test_map_img.c_str());
+}
+
+TEST_CASE("Obstacle Map Distance Testing Limited",
+          "[ObstMap][ObstMap_Distance]") {
+    // some setup code
+    string test_map_img = generate_image();
+
+    ObstMapUUT uut(test_map_img.c_str());
+    REQUIRE(uut.getHeight() == 600);
+    REQUIRE(uut.getWidth() == 600);
+
+    //Edge Case 1
+    CHECK(uut.distToObstacleLimited(300, 300, 2.356, 424) == -8.0);
+
+    //Edge Case 2
+    CHECK(uut.distToObstacleLimited(300, 300, 0.785, 423) == Approx(422.849855).epsilon(0.001));
+
+    //Quadrant 1
+    CHECK(uut.distToObstacleLimited(300, 300, 0.56, 93) == -8.0);
+    CHECK(uut.distToObstacleLimited(300, 300, 0.465, 224) == Approx(223.60679774).epsilon(0.001));
+    
+    //Quadrant 2
+    CHECK(uut.distToObstacleLimited(300, 300, 2.39, 219) == -8.0);
+    CHECK(uut.distToObstacleLimited(300, 300, 1.95, 270) == Approx(269.25824035).epsilon(0.001));
+    
+    //Quadrant 3
+    CHECK(uut.distToObstacleLimited(300, 300, 3.59, 277) == -8.0);
+    CHECK(uut.distToObstacleLimited(300, 300, 4.66, 201) == Approx(200.24984395).epsilon(0.001));
+    
+    //Quadrant 4
+    CHECK(uut.distToObstacleLimited(300, 300, 4.82, 135) == -8.0);
+    CHECK(uut.distToObstacleLimited(300, 300, 6.215, 251) == Approx(250.57733337).epsilon(0.001));
+    
+    remove(test_map_img.c_str());
 }
 
 TEST_CASE("Obstacle Map File Loading - 1000x100 all white",
@@ -123,6 +265,35 @@ TEST_CASE("Obstacle Map File Loading - 100x1000 checkerboard",
     REQUIRE(uut.getHeight() == 1000);
     REQUIRE(uut.getWidth() == 100);
     REQUIRE(verify_match(uut, data));
+
+    delete[] data;
+    remove(test_map_img);
+}
+
+TEST_CASE("Obstacle Map File Loading Flat - 100x1000 checkerboard",
+          "[ObstMap][ObstMap_Loading]") {
+    // some setup code
+    const char *test_map_img = "omap_loading_test.png";
+    size_t data_size = 100*1000;
+    char *data = new char[data_size];
+    // generate checkerboard
+    for(size_t r = 0; r < 1000; ++r) {
+        for(size_t c = 0; c < 100; ++c) {
+            // 25x100 checkbaord suqares, starting white and then alternating
+            if(((c/25) % 2 == 1 && (r/100) % 2 == 1)
+               || ((c/25) % 2 != 1 && (r/100) % 2 != 1)) {
+                data[(r*100) + c] = 0;
+            } else {
+                data[(r*100) + c] = static_cast<char>(255);
+            }
+        }
+    }
+    stbi_write_png(test_map_img, 100, 1000, 1, data, 100*1);
+
+    ObstMapUUT uut(test_map_img);
+    REQUIRE(uut.getHeight() == 1000);
+    REQUIRE(uut.getWidth() == 100);
+    REQUIRE(verify_match_flat(uut, data));
 
     delete[] data;
     remove(test_map_img);
