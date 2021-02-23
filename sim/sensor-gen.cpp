1466d08cbdef31db02deda8c38a67922a337871d
jocelynhsu8 <jocelynhsu8@gmail.com>
2021-02-22 16:11:38 -0800

    changed to decsion tree with prob

--- sim/sensor-gen.cpp
+++ sim/sensor-gen.cpp
@@ -1,55 +1,60 @@
 #include "sensor-gen.hpp"
 
-#include "obstacle_map.hpp"
-#include <random.h>
-#include <math.h>
+#include <random>
+#include <cmath>
 
-BeamModel::BeamModel(const Obst_Map& map, double std_dev, double lambda, double max_reading,
+#include "obstacle_map.hpp"
+
+
+BeamModel::BeamModel(double std_dev, double lambda, double max_reading,
                      double w_exp, double w_rand) :
     std_dev{std_dev},
     lambda{lambda},
     max_reading{max_reading},
-    w_exp{w_exp},
-    w_rand{w_rand} {
+    p_exp{w_exp},
+    p_rand{w_rand} {
         // constructor body
         // need to initialize w_hit and w_max
-        // w_hit = 1 - w_rand - w_exp;
-        w_max = 1 - w_hit - w_rand - w_exp;
+        // w_hit = 1 - w_rand - w_exp; eliminate; only weigh prob of noise and glitching
+        p_max = 1 - p_rand - p_exp;
 }
 
-Length BeamModel::sample(Length dist) {
+Length BeamModel::sampleNormal(Length dist) {
     srand(1);
+    // add dist to private class fields
     std::default_random_engine gen;
+    std::mt19937 gen{rd()}; // exp_dist(gen) to sample single number for exp_dist
     std::exponential_distribution<double> exp_dist(lambda);
-    std::normal_distribution<double> norm_dist(mean, std_dev); // need mean in param?
+    std::normal_distribution<double> norm_dist(dist, std_dev);
     std::uniform_real_distribution<double> uniform_dist(0,max_reading);
     
     double rand_exp = exp_dist(gen);
     double rand_norm = norm_dist(gen);
     double rand_uniform = uniform_dist(gen);
-    return  w_exp*rand_exp + w_hit*rand_norm + w_max*max_reading + w_rand*rand_uniform;
+    return  p_exp*rand_exp + p_hit*rand_norm + p_max*max_reading + p_rand*rand_uniform;
 }
 
-SensorGen::SensorGen(Obst_Map& map) :
+SensorGen::SensorGen(ObstMap& map) :
     map(map) {}
 
-SensorGen::SensorGen(Obst_Map& map, BeamModel& mdl) :
+SensorGen::SensorGen(ObstMap& map, BeamModel& mdl) :
     map(map),
     beam(mdl) {}
 
-Lidar::Lidar(Obst_Map& map, BeamModel& mdl) :
+// all sensors: x0 y0 center of sensor, range of angles in test
+Lidar::Lidar(ObstMap& map, BeamModel& mdl) :
     SensorGen(map, mdl) {}
 
 void Lidar::generate(Length *readings, Length x, Length y) {
     *readings = sqrt(pow(x,2) + pow(y,2));
 }
 
-UltrasoundSensor::UltrasoundSensor(Obst_Map& map, BeamModel& mdl) :
+UltrasoundSensor::UltrasoundSensor(ObstMap& map, BeamModel& mdl) :
     SensorGen(map, mdl) {}
 
 Length UltrasoundSensor::generate(Length x, Length y, Angle fov, Angle heading) {
     return cos(heading) * sqrt(pow(x,2)/pow(sin(fov),2)-pow(y,2));
 }
 
-IRSensor::IRSensor(Obst_Map& map, BeamModel& mdl) :
+IRSensor::IRSensor(ObstMap& map, BeamModel& mdl) :
     SensorGen(map, mdl) {}
--- sim/sensor-gen.hpp
+++ sim/sensor-gen.hpp
@@ -17,7 +17,7 @@
 #define SENSOR_GEN
 /* Need to include obstacle map header file */ //#include ""
 // forward declare obstaclemap type
-class Obst_Map;
+class ObstMap;
 // typedefs
 typedef unsigned Length;
 typedef unsigned Angle;
@@ -25,6 +25,7 @@ typedef unsigned Angle;
 /**
  * Class that defines a beammodel
  *
+ * add paragraph about statistical engine calculation
  * Note that the trivial copy construuctor/assignment operator is used which
  * will assign fields element by element. If additional pointer are added as
  * fields, this will need to be overridden
@@ -36,10 +37,11 @@ private:
     double max_reading; /**< recip. prob for the unifgorm dist. used for glitches. This
                              is also the maximum possible reading */
     // probability weight of each of the 4 cases
-    double w_exp;  /**< weight of the exponential component */
-    double w_hit;  /**< weight of the gaussian component */
-    double w_max;  /**< weight of the probability of gatting a max value reading*/
-    double w_rand; /**< weight of the uniform distribution */
+    double p_exp;  /**< probability of the exponential component */
+    //double p_hit;  /**< probability of the gaussian component */
+    //double p_max;  /**< probability of getting a max value reading*/
+    double p_rand; /**< probability of the uniform distribution */
+
 public:
     /**
      * Constructor to create a new BeamModel
@@ -51,26 +53,43 @@ public:
      * @param w_exp weight of exponential component
      * @param w_rand wight of the uniform component
      */
-    BeamModel(const Obst_Map& map, double std_dev, double lambda, double max_reading,
+    BeamModel(double std_dev, double lambda, double max_reading,
               double w_exp, double w_rand);
     /*
      * Sample sensor reading from distribution described by statistics.
      * @param dist true distance to obstacle
      * @return float sampled from distribution
      */
-    Length sample(Length dist);
+    Length sampleNormal(Length dist); // 
+    Length sampleGlitch(Length dist); // obstacle map --> within range, within dist + gaussian noise; else max value
+    bool glitch(); // call first -> if T call sampleGlitch else call sampleNormal; ultracount sampleNorm 15 deg, Lidar 720 deg
 };
 
 /**
- * baseclass for all sensor data generation functions
+ * Class that defines a beammodel (edit for sensorgen)
+ *
+ * Note that the trivial copy construuctor/assignment operator is used which
+ * will assign fields element by element. If additional pointer are added as
+ * fields, this will need to be overridden
  */
 class SensorGen {
 private:
-    BeamModel beam; /**< internal beammodel used by the sensor */
-    Obst_Map& map;  /**< obstacle map referenced by the sensor */
+   BeamModel model;
+   ObstMap map; // 
+
 public:
-    SensorGen(Obst_Map& map);
-    SensorGen(Obst_Map& map, BeamModel& mdl);
+    /**
+     * Constructor to create a new BeamModel
+     *
+     * @param map obstacle map to use to intialize values
+     * @param std_dev standard deviation fo sensor gaussian
+     * @param lambda parameter for exponential distribution
+     * @param max_reading maximum reading of sensor
+     * @param w_exp weight of exponential component
+     * @param w_rand wight of the uniform component
+     */
+    SensorGen(const ObstMap& map, double std_dev, double lambda, double max_reading,
+              double w_exp, double w_rand);
 };
 
 /**
@@ -78,7 +97,7 @@ public:
  */
 class Lidar : public SensorGen {
 public:
-    Lidar(Obst_Map& map, BeamModel& mdl);
+    Lidar(ObstMap& map, BeamModel& mdl);
     /**
      * Generate Lidar readings and write to array. (Use BeamModel in lieu of sensor statistics)
      * @param readings pointer to array to write Lidar readings to
@@ -93,7 +112,7 @@ public:
  */
 class UltrasoundSensor : public SensorGen {
 public:
-    UltrasoundSensor(Obst_Map& map, BeamModel& mdl);
+    UltrasoundSensor(ObstMap& map, BeamModel& mdl);
     /**
      * Generate ultrasound reading. (Use BeamModel in lieu of sensor statistics)
      *   @param x x coordinate of sensor in obstacle map
@@ -110,7 +129,7 @@ public:
  */
 class IRSensor : public SensorGen {
 public:
-    IRSensor(Obst_Map& map, BeamModel& mdl);
+    IRSensor(ObstMap& map, BeamModel& mdl);
     /**
      * Generate ultrasound reading. (Use BeamModel in lieu of sensor statistics)
      *   @param map obstacle map reference
