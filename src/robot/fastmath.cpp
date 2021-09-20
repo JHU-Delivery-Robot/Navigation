efe87ce0a57e7dc6aa7d47b26b3464dc5fdd8d19
Brendan Burkhart <brendan.f.burkhart@gmail.com>
2021-09-20 16:50:31 -0400

    Remove unused fastmath

--- src/robot/fastmath.cpp
+++
@@ -1,8 +0,0 @@
-/* implementation file for fast math library replacement functions that may rely
- * on hardware specific features
- */
-
-/* dummy content to suppress warning. remove when first implementation is added
- * to this file
- */
-typedef int make_iso_compilers_happy;
