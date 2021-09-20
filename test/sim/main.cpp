cff9653bd6590b5df491ca99be4e555b5ab4af62
Brendan Burkhart <brendan.f.burkhart@gmail.com>
2021-09-19 23:13:57 -0400

    Add failing test to verify action

--- test/sim/main.cpp
+++ test/sim/main.cpp
@@ -1,2 +1,10 @@
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
+
+#include <string>
+
+TEST_CASE("Fail")
+{
+    std::string *unfreed = new std::string("Allocated on heap, not freed");
+    REQUIRE(unfreed == nullptr); // fail on purpose
+}
