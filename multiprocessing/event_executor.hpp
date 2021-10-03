1efe088c6a3cedc810fee8c07f95fee21c04f1c2
aarenclwong <aarenclwong@gmail.com>
2021-10-03 11:36:00 -0400

    Make EventExecutorEvent methods pure virtual

--- multiprocessing/event_executor.hpp
+++ multiprocessing/event_executor.hpp
@@ -11,23 +11,23 @@ public:
     * Register a loop that will start executing as soon as the
     * Executor is started. Each loop is guaranteed to be concurrent.
     */
-    virtual void registerLoop(Runnable &loop, void *data);
+    virtual void registerLoop(Runnable &loop, void *data) = 0;
     /**
     * Register an event to fire on an interval. Note that this may consume
     * extra concurrency units, so be careful with use.
     */
-    virtual void registerTimedEvent(std::chrono::duration interval, Event event);
+    virtual void registerTimedEvent(std::chrono::duration interval, Event event) = 0;
     /**
     * Convenience function to register a handler that is executed
     * regularly at a defined interval
     * @param interval time between executions
     * @param handler handling code to run once the time is up
     */
-    virtual void registerTimedHandler(TimeInterval interval, Runnable &handler);
+    virtual void registerTimedHandler(std::chrono::duration interval, Runnable &handler) = 0;
     /**
     * Register a handler that fires on a given Event.
     */
-    virtual void registerEventHandler(Runnable &handler, Event event);
+    virtual void registerEventHandler(Runnable &handler, Event event) = 0;
     /**
     * Start the execution loops.
     */
