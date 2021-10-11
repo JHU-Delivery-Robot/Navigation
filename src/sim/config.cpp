c3ecfd59bbf1c368ca07780ab6aed2caf465e60e
Brendan Burkhart <brendan.f.burkhart@gmail.com>
2021-10-11 17:35:42 -0400

    Rudimentary sim config loading

---
+++ src/sim/config.cpp
@@ -0,0 +1,99 @@
+#include "config.hpp"
+
+#include <fstream>
+#include <iostream>
+#include <string>
+
+namespace sim {
+
+std::string trim(std::string s) {
+    auto left_it = std::find_if(s.begin(), s.end(), [](unsigned char ch) { return !std::isspace<char>(ch, std::locale("")); });
+    s.erase(s.begin(), left_it);
+
+    auto right_it = std::find_if(s.rbegin(), s.rend(), [](unsigned char ch) { return !std::isspace<char>(ch, std::locale("")); });
+    s.erase(right_it.base(), s.end());
+
+    return s;
+}
+
+common::Vector2 parse_point(std::string s) {
+    std::string x_str = s.substr(s.find('(') + 1, s.find(','));
+    std::string y_str = s.substr(s.find(',') + 1, s.find(')'));
+
+    double x = std::stod(x_str);
+    double y = std::stod(y_str);
+
+    return common::Vector2(x, y);
+}
+
+sim::Polygon parse_polygon(std::string s) {
+    size_t last = 0;
+    size_t next = 0;
+
+    std::vector<common::Vector2> points;
+
+    while ((next = s.find(')', last)) != std::string::npos) {
+        common::Vector2 point = parse_point(s.substr(last, next - last));
+        points.push_back(point);
+
+        if (s.find(',', next) == std::string::npos) {
+            break;
+        }
+
+        last = s.find(',', next) + 1;
+    }
+
+    return sim::Polygon(points);
+}
+
+std::optional<Config> Config::load(const std::filesystem::path& config_file_path) {
+    Config config;
+
+    std::ifstream config_file = std::ifstream(config_file_path);
+    if (!config_file.is_open()) {
+        std::cerr << "Failed to open config file" << std::endl;
+        return {};
+    }
+
+    std::string line;
+    while (std::getline(config_file, line)) {
+        size_t comma_index = line.find(',');
+        if (comma_index == std::string::npos) {
+            continue;
+        }
+
+        std::string key = trim(line.substr(0, comma_index));
+        std::string value = trim(line.substr(comma_index + 1));
+
+        if (key == "end_distance") {
+            config.end_distance = std::stod(value);
+        } else if (key == "time_step") {
+            config.time_step = std::stod(value);
+        } else if (key == "iteration_limit") {
+            config.iteration_limit = std::stoi(value);
+        } else if (key == "map_size") {
+            config.map_size = std::stoi(value);
+        } else if (key == "q_star") {
+            config.qStar = std::stod(value);
+        } else if (key == "attractive_gradient_scale") {
+            config.attractive_gradient_scale = std::stod(value);
+        } else if (key == "repulsive_gradient_scale") {
+            config.repulsive_gradient_scale = std::stod(value);
+        } else if (key == "start_angle") {
+            config.start_angle = std::stod(value) * 2 * PI / 180.0;
+        } else if (key == "start_position") {
+            config.start_position = parse_point(value);
+        } else if (key == "goal_position") {
+            config.goal_position = parse_point(value);
+        } else if (key == "obstacles") {
+            config.obstacles.push_back(parse_polygon(value));
+        } else {
+            std::cerr << "Unrecognized sim config key: " << key << std::endl;
+            return {};
+        }
+    }
+
+    return config;
+}
+
+}  // namespace sim
--- src/sim/main.cpp
+++ src/sim/main.cpp
@@ -1,48 +1,63 @@
+#include <filesystem>
+#include <fstream>
 #include <iomanip>
 #include <iostream>
-#include <fstream>
+#include <optional>
 
+#include "config.hpp"
 #include "hal_provider_sim_impl.hpp"
 #include "obstacle_map.hpp"
 #include "physics_sim.hpp"
 #include "polygon.hpp"
 #include "robot.hpp"
 
-int main () {
-    sim::Polygon obstacle1 = sim::Polygon({ common::Vector2(150, 100), common::Vector2(400, 100), common::Vector2(400, -100), common::Vector2(150, -100) });
-    sim::Polygon obstacle2 = sim::Polygon({ common::Vector2(-150, 100), common::Vector2(-400, 100), common::Vector2(-400, -100), common::Vector2(-150, -100) });
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        std::cerr << "Please specify config file path as only argument" << std::endl;
+        return 1;
+    }
 
-    sim::HALProviderSimImpl sim_hal({ obstacle1, obstacle2 });
+    std::filesystem::path config_file_path = std::filesystem::path(argv[1]);
+    if (!std::filesystem::exists(config_file_path)) {
+        std::cerr << "Can't find specified config file, please double-check path" << std::endl;
+        return 1;
+    }
+
+    sim::Config config;
+    std::cout << "Loading config... ";
+    if (auto config_opt = sim::Config::load(config_file_path)) {
+        config = config_opt.value();
+        std::cout << "Config loaded successfully" << std::endl;
+    } else {
+        std::cerr << "Failed to load config" << std::endl;
+        return 1;
+    }
+
+    sim::HALProviderSimImpl sim_hal(config.obstacles);
     sim::PhysicsSim physics = sim::PhysicsSim(&sim_hal);
 
-    common::Vector2 start = common::Vector2(-300.0, -200.0);
-    common::Vector2 goal = common::Vector2(350.0, 300.0);
-
-    physics.setPose(start, 0.5*PI);
+    physics.setPose(config.start_position, config.start_angle);
 
     robot::Robot robot = robot::Robot(&sim_hal);
 
-    constexpr double min_distance = 20;
-    constexpr double time_step = 0.02; // 50 Hz
-    constexpr double time_limit = 60;
-
     std::ofstream sim_output_file("sim_output.json");
-    if (!sim_output_file.is_open())
-    {
+    if (!sim_output_file.is_open()) {
         std::cout << "Couldn't open log file";
         return 1;
     }
 
     sim_output_file << R"({"size": 800, "time_step": 0.02, "goal": [350.0, 300.0], "obstacles": [)"
-                  << R"([ [ 150, 100 ], [ 400, 100 ], [ 400, -100 ], [ 150, -100 ] ],)"
-                  << R"([ [ -150, 100 ], [ -400, 100 ], [ -400, -100 ], [ -150, -100 ] ] ],)"
-                  << R"("positions": [ [-300.0, -200.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0] )";
+                    << R"([ [ 150, 100 ], [ 400, 100 ], [ 400, -100 ], [ 150, -100 ] ],)"
+                    << R"([ [ -150, 100 ], [ -400, 100 ], [ -400, -100 ], [ -150, -100 ] ] ],)"
+                    << R"("positions": [ [-300.0, -200.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0] )";
 
-    robot::GradientPotentialMap potential_map_parallel_copy = robot::GradientPotentialMap(600, 0.08,  1E5, goal);
-    physics.update(0.0); // initialize physics
+    robot::GradientPotentialMap potential_map_parallel_copy = robot::GradientPotentialMap(config.qStar, config.attractive_gradient_scale, config.repulsive_gradient_scale, config.goal_position);
 
-    double time = 0.0;
-    while (time < time_limit) {
+    // Initialize physics
+    physics.update(0.0);
+
+    int current_iteration = 0;
+    while (current_iteration++ < config.iteration_limit) {
         robot.update();
 
         auto [initial_position, initial_heading] = physics.getPose();
@@ -52,18 +67,17 @@ int main () {
         auto attractiveGradient = potential_map_parallel_copy.getAttractivePotential(lidar_position);
         auto repulsiveGradient = potential_map_parallel_copy.getRepulsivePotential();
 
-        physics.update(time_step);
-        time += time_step;
+        physics.update(config.time_step);
 
         auto [position, heading] = physics.getPose();
-        double distance_to_goal = (position - goal).magnitude();
+        double distance_to_goal = (position - config.goal_position).magnitude();
 
-        if (distance_to_goal <= min_distance) {
+        if (distance_to_goal <= config.end_distance) {
             break;
         }
 
-        double left_speed = 0.5*(sim_hal.motor_assembly()->front_left()->get_speed() + sim_hal.motor_assembly()->back_left()->get_speed());
-        double right_speed = 0.5*(sim_hal.motor_assembly()->front_right()->get_speed() + sim_hal.motor_assembly()->back_right()->get_speed());
+        double left_speed = 0.5 * (sim_hal.motor_assembly()->front_left()->get_speed() + sim_hal.motor_assembly()->back_left()->get_speed());
+        double right_speed = 0.5 * (sim_hal.motor_assembly()->front_right()->get_speed() + sim_hal.motor_assembly()->back_right()->get_speed());
 
         sim_output_file << ",\n[ " << position.x << ", " << position.y << ", " << heading << ", " << left_speed << ", " << right_speed << ", " << attractiveGradient.x << ", " << attractiveGradient.y << ", " << repulsiveGradient.x << ", " << repulsiveGradient.y << " ]";
     }
