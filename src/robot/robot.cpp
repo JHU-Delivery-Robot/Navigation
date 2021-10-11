a312d6188f18cce3cac517aa1adb25a05df0a830
Brendan Burkhart <brendan.f.burkhart@gmail.com>
2021-10-11 17:35:55 -0400

    Formatting

--- src/robot/robot.cpp
+++ src/robot/robot.cpp
@@ -9,7 +9,7 @@ Robot::Robot(hal::HALProvider* hal)
     : hal(hal),
       drivetrain(hal->motor_assembly(), hal->gyroscope()),
       potential_map(600, 0.08, 1E5, common::Vector2(500, 500)) {
-    drivetrain.setPose(common::Vector2(-300.0, -200.0), 0.0*PI);
+    drivetrain.setPose(common::Vector2(-300.0, -200.0), 0.0 * PI);
     potential_map.updateGoal(common::Vector2(350.0, 300.0));
 
     hal->motor_assembly()->reset_odometry();
@@ -22,13 +22,13 @@ void Robot::update() {
     auto lidar_scan = hal->lidar()->read();
     potential_map.updateLidar(lidar_scan, heading);
 
-    common::Vector2 gradient = potential_map.getGradient(position + common::Vector2::polar(heading, 0.5*drivetrain.length));
+    common::Vector2 gradient = potential_map.getGradient(position + common::Vector2::polar(heading, 0.5 * drivetrain.length));
 
     if (gradient.magnitude() >= 500.0) {
-        gradient = gradient * (500.0/gradient.magnitude());
+        gradient = gradient * (500.0 / gradient.magnitude());
     }
 
     drivetrain.setCommand(gradient);
 }
 
-}
+}  // namespace robot
