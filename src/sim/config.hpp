bea629c5ca7ee3ae1a33f890f2981c656500841f
Brendan Burkhart <brendan.f.burkhart@gmail.com>
2021-10-11 17:35:42 -0400

    Rudimentary sim config loading

---
+++ src/sim/config.hpp
@@ -0,0 +1,36 @@
+#ifndef SIM_CONFIG_HPP
+#define SIM_CONFIH_HPP
+
+#include <filesystem>
+#include <optional>
+#include <vector>
+
+#include "common.hpp"
+#include "polygon.hpp"
+#include "vector2.hpp"
+
+namespace sim {
+
+class Config {
+public:
+    static std::optional<Config> load(const std::filesystem::path& config_file_path);
+
+    double end_distance = 20;
+    double time_step = 0.02;
+    int iteration_limit = 60 * 50;
+    int map_size = 800;
+
+    double qStar;
+    double attractive_gradient_scale;
+    double repulsive_gradient_scale;
+
+    common::Vector2 start_position = common::Vector2(-300.0, -200.0);
+    double start_angle = 0.5 * PI;
+    common::Vector2 goal_position = common::Vector2(350.0, 300.0);
+
+    std::vector<sim::Polygon> obstacles;
+};
+
+}  // namespace sim
+
+#endif
