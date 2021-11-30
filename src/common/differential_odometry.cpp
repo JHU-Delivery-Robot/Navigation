2f3e617ef34aded66bb1ca1f511059eb64ca8faf
Brendan Burkhart <brendan.f.burkhart@gmail.com>
2021-11-30 12:53:50 -0500

    Unit tests for odometry
    - Move forward kinematics for rotation from physics_sim to
      differential_odometry
    - Add unit tests for differential_odometry

--- src/common/differential_odometry.cpp
+++ src/common/differential_odometry.cpp
@@ -1,61 +1,72 @@
 #include "differential_odometry.hpp"
 
 #include <cmath>
+#include <iostream>
 
 #include "common.hpp"
 
-namespace common
-{
+namespace common {
 
-DifferentialOdometry::DifferentialOdometry(double width, double wheel_circumference)
-    : wheel_base_width(width), wheel_circumference(wheel_circumference), position(0.0, 0.0), heading(0.0) {}
+DifferentialOdometry::DifferentialOdometry(double width)
+    : wheel_base_width(width),
+      position(0.0, 0.0),
+      heading(0.0),
+      left_wheels_distance(0.0),
+      right_wheels_distance(0.0) {}
 
-void DifferentialOdometry::setPose(common::Vector2 position, double heading)
-{
+void DifferentialOdometry::setPose(common::Vector2 position, double heading) {
     this->position = position;
     this->heading = heading;
 }
 
-std::tuple<common::Vector2, double> DifferentialOdometry::getPose()
-{
+std::tuple<common::Vector2, double> DifferentialOdometry::getPose() {
     return {position, heading};
 }
 
-void DifferentialOdometry::updateOdometry(double heading, double left_wheels_distance, double right_wheels_distance)
-{
-    double deltaLeft = left_wheels_distance - this->left_wheels_distance;
-    double deltaRight = right_wheels_distance - this->right_wheels_distance;
-    double deltaAngle = heading - this->heading;
+void DifferentialOdometry::updateOdometry(double heading, double total_left_distance, double total_right_distance) {
+    double delta_left = total_left_distance - this->left_wheels_distance;
+    double delta_right = total_right_distance - this->right_wheels_distance;
+    double delta_angle = heading - this->heading;
 
-    this->left_wheels_distance = left_wheels_distance;
-    this->right_wheels_distance = right_wheels_distance;
+    this->left_wheels_distance = total_left_distance;
+    this->right_wheels_distance = total_right_distance;
     this->heading = heading;
 
-    double distance = (deltaLeft + deltaRight) / 2.0;
+    double distance = (delta_left + delta_right) / 2.0;
 
-    if (std::abs(deltaAngle) > PI)
-    {
-        deltaAngle = copysign(2.0 * PI - std::abs(deltaAngle), -deltaAngle);
+    // Keep delta_angle in range [-PI, PI]
+    if (std::abs(delta_angle) > PI) {
+        delta_angle = copysign(2.0 * PI - std::abs(delta_angle), -delta_angle);
+        std::cout << delta_angle << std::endl;
     }
 
-    if (std::abs(deltaAngle) < 1e-4)
-    {
-        double deltaX = distance * cos(heading);
-        double deltaY = distance * sin(heading);
+    if (std::abs(delta_angle) < 1e-4) {
+        double delta_x = distance * cos(heading);
+        double delta_y = distance * sin(heading);
 
-        position += common::Vector2(deltaX, deltaY);
+        position += common::Vector2(delta_x, delta_y);
 
         return;
     }
 
-    double radius = distance / std::abs(deltaAngle);
-    double chordAngle = heading - deltaAngle / 2.0; // average angle over turn
-    double chordLength = 2.0 * radius * sin(std::abs(deltaAngle) / 2.0);
+    double radius = distance / delta_angle;
+    double chord_angle = heading - delta_angle / 2.0;
+    double chord_length = std::abs(2.0 * radius * sin(delta_angle / 2.0));
 
-    double deltaX = chordLength * cos(chordAngle);
-    double deltaY = chordLength * sin(chordAngle);
+    double delta_x = chord_length * cos(chord_angle);
+    double delta_y = chord_length * sin(chord_angle);
 
-    position += common::Vector2(deltaX, deltaY);
+    position += common::Vector2(delta_x, delta_y);
 }
 
+double DifferentialOdometry::estimateRotation(double total_left_distance, double total_right_distance) const {
+    double delta_left = total_left_distance - this->left_wheels_distance;
+    double delta_right = total_right_distance - this->right_wheels_distance;
+
+    double diff = delta_right - delta_left;
+
+    double delta_theta = diff / wheel_base_width;
+    return delta_theta;
 }
+
+}  // namespace common
--- src/common/differential_odometry.hpp
+++ src/common/differential_odometry.hpp
@@ -7,24 +7,88 @@
 
 namespace common {
 
+/**
+ * DifferentialOdometry provides a kinematic model of differential drivetrains
+ * for odometry. A given instance tracks the distances the wheels of a robot
+ * have driven, and calculates where the robot is based on the wheel distances.
+ *
+ * Each update to the odometry is fed the current heading of the robot and the
+ * total distances the wheels have drive, and uses this to estimate how far and
+ * in what direction the robot has driven since the last update. The heading
+ * is ideally provided by a gyroscope or compass. However this odometry model
+ * is capable of estimating the rotation of the robot since the last update
+ * based solely on wheel distances, which can replace a gyroscope if needed.
+ *
+ * For best results, the odometry should be updated as frequently as feasible.
+ * Around 50 times per second will generally give good results. Note that the
+ * position estimate provided by odometry will necessarily drift from the
+ * robot's actual position over time due to momentary losses of traction and
+ * other factors.
+ *
+ * The units chosen for distance are arbitrary and simply need to be chosen
+ * consistently. All angles are in radians, and measured increasing
+ * anti-clockwise and starting with zero being the x-axis.
+ */
 class DifferentialOdometry {
 public:
-    DifferentialOdometry(double width, double wheel_circumference);
+    /**
+     * Constructs a model of a specific differential drive train.
+     *
+     * @param width The wheel base width - the total distance between
+     * the left and right wheels. This should be in the same units
+     * as the wheel distances will be provided in.
+     */
+    DifferentialOdometry(double width);
 
+    /**
+     * Set the robot's pose, useful for initializing odometry.
+     *
+     * @param position The position the robot is currently at.
+     * @param heading The robot's current heading angle in radians.
+     */
     void setPose(common::Vector2 position, double heading);
+
+    /**
+     * Get the robot's current pose.
+     *
+     * @return Position and heading angle in radians.
+     */
     std::tuple<common::Vector2, double> getPose();
 
-    void updateOdometry(double heading, double left_distance, double right_distance);
+    /**
+     * Update the estimate of the robot's pose using the kinematic model.
+     * For best results, update odometry frequently.
+     *
+     * @param heading The current heading angle of the robot.
+     * @param total_left_distance Total distance driven by left wheels.
+     * @param total_right_distance Total distance driven by right wheels.
+     */
+    void updateOdometry(double heading, double total_left_distance, double total_right_distance);
+
+    /**
+     * Estimate the rotation of the robot since the last odometry update, based
+     * on the distances the wheels have driven. This does not change the estimated
+     * pose at all.
+     *
+     * Provided as an alternative method of tracking heading if a gyroscopic sensor
+     * or compass is unavailable, or for modelling heading changes in a simulation.
+     * The estimated rotation can simply be added to a running estimate of the robot's
+     * heading before each call to updateOdometry. If this method is chosen rather
+     * than a gyroscope, the estimated heading will drift over time.
+     *
+     * @param total_left_distance Total distance driven by left wheels.
+     * @param total_right_distance Total distance driven by right wheels.
+     */
+    double estimateRotation(double total_left_distance, double total_right_distance) const;
 
 private:
     double wheel_base_width;
-    double wheel_circumference;
 
     common::Vector2 position;
     double heading;
     double left_wheels_distance, right_wheels_distance;
 };
 
-}
+}  // namespace common
 
 #endif
