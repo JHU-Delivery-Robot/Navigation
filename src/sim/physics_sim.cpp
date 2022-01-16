412c7d438a68bc4028511f3fb105735a4f3911bb
Brendan Burkhart <brendan.f.burkhart@gmail.com>
2022-01-15 18:14:35 -0800

    Fix merge errors

--- src/sim/physics_sim.cpp
+++ src/sim/physics_sim.cpp
@@ -10,30 +10,14 @@ PhysicsSim::PhysicsSim(HALProviderSimImpl *sim_hal)
     : sim_hal(sim_hal),
       odometry(robot::Drivetrain::wheel_base_width) {}
 
-    double instantaneous_curve_radius = 0.5 * robot::Drivetrain::wheel_base_width * (sum / diff);
-
-    if (std::abs(instantaneous_curve_radius) < 1e-4) {
-        double delta_theta = 0.5*(delta_right_distance-delta_left_distance)/(PI*robot::Drivetrain::wheel_base_width);
-        return delta_theta;
-    }
-
-    double distance = speed * time_delta;
-    double delta_theta = distance / instantaneous_curve_radius;
-
-    return delta_theta;
-}
-
 void PhysicsSim::update(double time_delta) {
     HALProviderSimImpl::MotorAssembly *motors = sim_hal->motor_assembly();
 
     motors->update(time_delta);
 
-    double delta_theta = estimate_angle_rotation(time_delta);
-    sim_hal->gyroscope()->updateHeading(delta_theta);
-
     double total_left_distance = 0.5 * (motors->front_left()->odometry_distance() + motors->back_left()->odometry_distance());
     double total_right_distance = 0.5 * (motors->front_right()->odometry_distance() + motors->back_right()->odometry_distance());
-
+    
     double delta_theta = odometry.estimateRotation(total_left_distance, total_right_distance);
     sim_hal->gyroscope()->updateHeading(delta_theta);
 
