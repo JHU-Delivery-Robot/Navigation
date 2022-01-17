818d7c87cb19bca52c236c43bd6f3cd5fb0b6010
Brendan Burkhart <brendan.f.burkhart@gmail.com>
2022-01-17 11:31:23 -0800

    Remove unused files

--- src/robot/main.cpp
+++ src/robot/main.cpp
@@ -1,6 +1,3 @@
-#include "potential_map.hpp"
-#include "types.hpp"
-
 // no literal map (2D array representing grid), but we're constantly fed
 // robot's location and goal points
 // (either receive all goal points at once, or one at a time as the robot moves)
