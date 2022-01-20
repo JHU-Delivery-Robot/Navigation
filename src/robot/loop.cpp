908d8e4d3afe6ce101621fcd8e8010afc6b25b88
rbiz4 <rbis1001@gmail.com>
2022-01-19 20:54:23 -0500

    Completed multiprocessing implementation Changed the hal provider implementation to take lidar configs, will expand to other sensor configs later as needed Implemented robot loop Kicked off lidar loop

--- src/robot/loop.cpp
+++ src/robot/loop.cpp
@@ -1,16 +1,21 @@
 #include "loop.hpp"
 
-Loop::Loop(int interval) : updateInterval(interval), shouldBeRunning(true) {}
+namespace robot {
 
-Loop::cancel() {
+Loop::Loop(int interval, std::function<void()> function)
+    : updateInterval(interval), shouldBeRunning(true), function(function) {}
+
+void Loop::cancel() {
     shouldBeRunning = false;
 }
 
-Loop::looping() {
+void Loop::looping() {
     while (shouldBeRunning) {
         lastLoopUpdate = clock.now();
-        doWork();
+        function();
         std::chrono::duration duration = clock.now() - lastLoopUpdate;
         std::this_thread::sleep_for(updateInterval - duration);
     }
+}
+
 }
