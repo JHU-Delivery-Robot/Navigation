a594dc9cb59a37326a0751e105fd78de3ff0f8fc
Brendan Burkhart <brendan.f.burkhart@gmail.com>
2022-01-20 18:36:52 -0800

    Clean up meson build
    Try to mostly use src/include variables rather than libaries, and
    declare dependencies when libraries are used.

--- src/sim/physics_sim.cpp
+++ src/sim/physics_sim.cpp
@@ -6,30 +6,18 @@
 
 namespace sim {
 
-PhysicsSim::PhysicsSim(HALProviderSimImpl *sim_hal)
-    : sim_hal(sim_hal),
+PhysicsSim::PhysicsSim(hal::HALProvider *hal)
+    : hal(hal),
       odometry(robot::Drivetrain::wheel_base_width) {}
 
-void PhysicsSim::update(double time_delta) {
-    HALProviderSimImpl::MotorAssembly *motors = sim_hal->motor_assembly();
-
-    motors->update(time_delta);
+void PhysicsSim::update() {
+    hal::HALProvider::MotorAssembly *motors = hal->motor_assembly();
 
     double total_left_distance = 0.5 * (motors->front_left()->odometry_distance() + motors->back_left()->odometry_distance());
     double total_right_distance = 0.5 * (motors->front_right()->odometry_distance() + motors->back_right()->odometry_distance());
 
     double delta_theta = odometry.estimateRotation(total_left_distance, total_right_distance);
-    sim_hal->gyroscope()->updateHeading(delta_theta);
-
-    odometry.updateOdometry(sim_hal->gyroscope()->angle(), total_left_distance, total_right_distance);
-
-    auto [position, heading] = odometry.getPose();
-
-    sim_hal->gps()->updateLocation(position, heading);
-    sim_hal->lidar()->updateLocation(position, heading);
-    sim_hal->cliff_infrared()->updateLocation(position, heading);
-    sim_hal->wheel_infrared()->updateLocation(position, heading);
-    sim_hal->ultrasonic()->updateLocation(position, heading);
+    odometry.updateOdometry(hal->gyroscope()->angle() + delta_theta, total_left_distance, total_right_distance);
 }
 
 std::tuple<common::Vector2, double> PhysicsSim::getPose() {
@@ -38,13 +26,6 @@ std::tuple<common::Vector2, double> PhysicsSim::getPose() {
 
 void PhysicsSim::setPose(common::Vector2 position, double heading) {
     odometry.setPose(position, heading);
-    sim_hal->gyroscope()->setHeading(heading);
-
-    sim_hal->gps()->updateLocation(position, heading);
-    sim_hal->lidar()->updateLocation(position, heading);
-    sim_hal->cliff_infrared()->updateLocation(position, heading);
-    sim_hal->wheel_infrared()->updateLocation(position, heading);
-    sim_hal->ultrasonic()->updateLocation(position, heading);
 }
 
 }  // namespace sim
