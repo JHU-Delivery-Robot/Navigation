358a9878b3e318e70dc69caa673c2060d87da44b
Brendan Burkhart <brendan.f.burkhart@gmail.com>
2022-05-13 17:22:02 -0400

    Improved periodic task executor
    - Improved periodic task executor, renamed from 'Loop' to 'Periodic'
    - Use atomic bools for inter-thread communication
    - Use Periodic for running server comms

--- src/comms/comms.cpp
+++ src/comms/comms.cpp
@@ -2,6 +2,8 @@
 
 #include <grpcpp/grpcpp.h>
 
+#include <functional>
+
 #include "common/vector2.hpp"
 
 namespace comms {
@@ -11,56 +13,48 @@ Comms::Comms(std::string server_url, events::RouteControl route_control, hal::Po
       stub(routing::Routing::NewStub(channel)),
       route_control(route_control),
       positioning(positioning),
-      comms_open(false) {}
+      poll_task(poll_interval_ms, std::bind(&Comms::poll, this)) {}
 
 bool Comms::open() {
-    comms_open = true;
-    comms_executor = std::async(std::launch::async, [&]() {
-        poll();
-    });
+    poll_task.start();
 
     return true;
 }
 
 bool Comms::close() {
-    comms_open = false;
+    poll_task.stop();
+
     return true;
 }
 
 void Comms::poll() {
-    while (comms_open) {
-        grpc::ClientContext context;
-        routing::Route route_response;
-        routing::RobotStatus status;
+    grpc::ClientContext context;
+    routing::Route route_response;
+    routing::RobotStatus status;
 
-        routing::Point location;
-        location.set_latitude(39.325702);
-        location.set_longitude(-76.621478);
+    routing::Point location;
+    location.set_latitude(39.325702);
+    location.set_longitude(-76.621478);
 
-        status.set_status(routing::RobotStatus::OK);
-        status.mutable_location()->CopyFrom(location);
+    status.set_status(routing::RobotStatus::OK);
+    status.mutable_location()->CopyFrom(location);
 
-        stub->GetRoute(&context, status, &route_response);
+    grpc::Status grpc_status = stub->GetRoute(&context, status, &route_response);
 
-        std::cout << "Route: ";
-
-        std::vector<common::Vector2> updated_route = std::vector<common::Vector2>();
-        for (int i = 0; i < route_response.waypoints_size(); i++) {
-            const routing::Point& waypoint_coordinates = route_response.waypoints(i);
-            const common::Vector2 waypoint = common::Vector2(waypoint_coordinates.longitude(), waypoint_coordinates.latitude());
-            std::cout << waypoint;
-
-            if (i < route_response.waypoints_size() - 1) {
-                std::cout << ", ";
-            }
-
-            updated_route.push_back(waypoint);
-        }
-
-        std::cout << std::endl;
-
-        route_control.updateRoute(updated_route);
+    if (!grpc_status.ok()) {
+        // if (grpc_status.error_code() == grpc::StatusCode::UNAVAILABLE)
+        std::cout << "Server comms error: " << grpc_status.error_code() << ": " << grpc_status.error_message() << std::endl;
+        return;
     }
+
+    std::vector<common::Vector2> updated_route = std::vector<common::Vector2>();
+    for (int i = 0; i < route_response.waypoints_size(); i++) {
+        const routing::Point& waypoint_coordinates = route_response.waypoints(i);
+        const common::Vector2 waypoint = common::Vector2(waypoint_coordinates.longitude(), waypoint_coordinates.latitude());
+        updated_route.push_back(waypoint);
+    }
+
+    route_control.updateRoute(updated_route);
 }
 
 }  // namespace comms
--- src/robot_main.cpp
+++ src/robot_main.cpp
@@ -1,47 +1,38 @@
 #include <functional>
 #include <thread>
 
+#include "common/periodic.hpp"
 #include "comms/comms.hpp"
 #include "events/event_queue.hpp"
 #include "events/route_control.hpp"
 #include "hal/robot_impl/hal_provider_impl.hpp"
-#include "robot/loop.hpp"
 #include "robot/robot.hpp"
 
 int main() {
-    //Initialize configs
-    hal::impl::HALProviderImpl::LidarConfig lidar_config =
-        hal::impl::HALProviderImpl::LidarConfig("COM3", 12.0);
+    hal::impl::HALProviderImpl::LidarConfig lidar_config = hal::impl::HALProviderImpl::LidarConfig("COM3", 12.0);
 
     events::EventQueue event_queue;
     events::ErrorReporting error_reporting(&event_queue);
     events::RouteControl route_control(&event_queue);
 
-    //Initialize hal provider implementation
     hal::impl::HALProviderImpl hal_provider = hal::impl::HALProviderImpl(lidar_config, error_reporting);
     hal_provider.initialize();
 
-    //Get Lidar
     hal::impl::TG30Lidar* lidar = hal_provider.lidar();
 
-    //Get Robot
-    robot::Robot rob = robot::Robot(&hal_provider, &event_queue);
+    robot::Robot robot(&hal_provider, &event_queue);
 
-    //Initialize Robot Loop
-    robot::Loop robot_loop = robot::Loop(10, std::bind(&robot::Robot::update, &rob));
+    common::Periodic navigation(10, std::bind(&robot::Robot::update, &robot));
 
     comms::Comms comms("127.0.0.1:9000", route_control, hal_provider.positioning());
 
-    //Start all threads
-    std::thread robot_thread(&robot::Loop::looping, &robot_loop);  //Robot Thread
-    lidar->beginScanning();                                        //Lidar Thread
+    // Start
+    lidar->beginScanning();
     comms.open();
+    navigation.start();
 
-    // TODO: Insert Stopping Function
-
-    //Stop all threads
-    robot_loop.cancel();
-    robot_thread.join();
+    // Stop
+    navigation.stop();
     lidar->stopScanning();
     comms.close();
 
