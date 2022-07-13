796a856e995edf1cad82de1e119fd6760b3e0f6c
Brendan Burkhart <brendan.f.burkhart@gmail.com>
2022-07-13 16:42:05 -0400

    Fix key bindings

--- src/sim/visualization/pages/config_creator/drawer.js
+++ src/sim/visualization/pages/config_creator/drawer.js
@@ -1,14 +1,9 @@
 const canvas = document.getElementById('visualizationCanvas');
 const container = document.getElementById('visualization-container');
 
-var ctx = canvas.getContext('2d');
 var drawing = false;
 const offsetX = canvas.offsetLeft;
 const offsetY = canvas.offsetTop;
-var cw = container.offsetWidth;
-ctx.canvas.width  = cw;
-var ch = container.offsetHeight;
-ctx.canvas.height = ch;
 var drawing = "p";
 var allObjects = [];
 var coordinates = [];
@@ -16,23 +11,24 @@ var redoElements = [];
 var coordsRedo = [];
 
 //DEFINE DRAWING OBJECT-------------------
-class DrawObject{
-    constructor(ctx, type, coords) {
-        this.ctx = ctx;
-        this.type= type;
+class DrawObject {
+    constructor(type, coords) {
+        this.type = type;
         this.coords = coords;
     }
 
     draw() {
-        switch(this.type){
+        let ctx = canvas.getContext('2d');
+
+        switch (this.type) {
             case "p":
                 ctx.beginPath();
                 ctx.moveTo(this.coords[0].x, this.coords[0].y);
-                for(var index=1; index<this.coords.length;index++) {
+                for (var index = 1; index < this.coords.length; index++) {
                     ctx.lineTo(this.coords[index].x, this.coords[index].y);
-                }   
+                }
                 ctx.closePath();
-                ctx.lineWidth=2;
+                ctx.lineWidth = 2;
                 ctx.fillStyle = 'black';
                 ctx.fill();
                 break;
@@ -45,7 +41,7 @@ class DrawObject{
                 break;
             case "s":
                 ctx.beginPath();
-                if(this.coords.length!=0){
+                if (this.coords.length != 0) {
                     ctx.arc(this.coords[0].x, this.coords[0].y, 10, 0, 2 * Math.PI, false);
                     ctx.fillStyle = 'green';
                     ctx.fill();
@@ -56,52 +52,50 @@ class DrawObject{
     }
 }
 
-var startPoint = new DrawObject(ctx, "s", []);
+var startPoint = new DrawObject("s", []);
 
 //---------------------------------
 
 //CLEAR AND REDRAW ALL OBJECTS
-function redrawAll(){
-    ctx.clearRect(0,0,cw,ch);
+function redrawAll() {
+    let ctx = canvas.getContext('2d');
+    ctx.fillStyle = '#ddd';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = '#000';
+
     startPoint.draw();
-    for(var i = 0; i<allObjects.length; i++){
+    for (var i = 0; i < allObjects.length; i++) {
         allObjects[i].draw();
     }
 }
 
-//UNDO
-window.onkeypress = function(e){
-    if(e.which === 90 && e.ctrlKey ){
-        if(coordinates.length!=0){
+document.addEventListener('keydown', function (e) {
+    //UNDO
+    if (e.key === 'z' && e.ctrlKey) {
+        if (coordinates.length != 0) {
             coordsRedo.push(coordinates.pop());
             redrawAll();
             drawPolygon(coordinates);
-        }
-        else{
+        } else {
             redoElements.push(allObjects.pop());
             redrawAll();
         }
-    }          
-};
-
-//REDO
-window.onkeypress = function(e){
-  if( e.which === 89 && e.ctrlKey ){
-    if(coordinates.length!=0 || coordsRedo.length!=0){
-      if(coordsRedo.length!=0){
-      coordinates.push(coordsRedo.pop());
-      redrawAll();
-      drawPolygon(coordinates);
-      }
+        //REDO
+    } else if (e.key === 'y' && e.ctrlKey) {
+        if (coordinates.length != 0 || coordsRedo.length != 0) {
+            if (coordsRedo.length != 0) {
+                coordinates.push(coordsRedo.pop());
+                redrawAll();
+                drawPolygon(coordinates);
+            }
+        } else {
+            if (redoElements.length > 0) {
+                allObjects.push(redoElements.pop());
+            }
+            redrawAll();
+        }
     }
-    else{
-      if(redoElements.length>0){
-        allObjects.push(redoElements.pop());
-      }
-      redrawAll();
-    }
-  }          
-}; 
+});
 
 function start_polygon() {
     drawing = "p";
@@ -130,26 +124,25 @@ function close_polygon() {
 
 //Handle canvas click operations-----------------------------
 function onCanvasClick(event) {
-    console.log(event);
     // tell the browser we're handling this event
     event.preventDefault();
     event.stopPropagation();
-    mouseX=parseInt(event.clientX-offsetX);
+    mouseX = parseInt(event.clientX - offsetX);
     mouseY = parseInt(event.clientY - offsetY);
 
-    if(drawing == "p") {
-        coordinates.push({x:mouseX,y:mouseY});
+    if (drawing == "p") {
+        coordinates.push({ x: mouseX, y: mouseY });
         drawPolygon(coordinates);
     }
-    else if(drawing == "w") {
-        coordinates.push({x:mouseX,y:mouseY});
-        waypoint = new DrawObject(ctx,"w", [...coordinates]);
+    else if (drawing == "w") {
+        coordinates.push({ x: mouseX, y: mouseY });
+        waypoint = new DrawObject(ctx, "w", [...coordinates]);
         allObjects.push(waypoint);
         waypoint.draw();
         coordinates = [];
     }
-    else if(drawing == "s") {
-        coordinates.push({x:mouseX,y:mouseY});
+    else if (drawing == "s") {
+        coordinates.push({ x: mouseX, y: mouseY });
         startPoint.coords = [...coordinates];
         redrawAll();
         coordinates = [];
@@ -175,25 +168,27 @@ function resizeCanvas() {
 
 //---------------------
 
-function drawPolygon(coords){
+function drawPolygon(coords) {
+    let ctx = canvas.getContext('2d');
+
     redrawAll();
     ctx.beginPath();
-    ctx.strokeStyle='black';
+    ctx.strokeStyle = 'black';
     ctx.fillStyle = 'black';
     ctx.moveTo(coords[0].x, coords[0].y);
 
-    for(index=1; index<coords.length;index++) {
+    for (index = 1; index < coords.length; index++) {
         ctx.lineTo(coords[index].x, coords[index].y);
     }
 
     ctx.closePath();
     ctx.stroke();
-    ctx.lineWidth=2;
+    ctx.lineWidth = 2;
 
     ctx.fill();
     ctx.fillStyle = 'red';
 
-    for(var i = 0; i<coords.length;i++) {
-        ctx.fillRect(coords[i].x-5,coords[i].y-5,10,10);
+    for (var i = 0; i < coords.length; i++) {
+        ctx.fillRect(coords[i].x - 5, coords[i].y - 5, 10, 10);
     }
 }
