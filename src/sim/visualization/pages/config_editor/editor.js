95bac06b6bcd834405846370d9bf580a36050a0a
Brendan Burkhart <brendan.f.burkhart@gmail.com>
2022-10-19 22:32:18 -0400

    Transform editor canvas to match sim coordinates

--- src/sim/visualization/pages/config_editor/editor.js
+++ src/sim/visualization/pages/config_editor/editor.js
@@ -2,7 +2,9 @@ const canvas = document.getElementById('visualizationCanvas');
 const container = document.getElementById('visualization-container');
 const configFileSelector = document.getElementById('configFileSelector');
 
-let config = {};
+let config = {
+    map_size: 8,
+};
 
 class Drawable {
     constructor(scene, type) {
@@ -10,6 +12,7 @@ class Drawable {
         this.type = type;
     }
 
+    draw(ctx) { }
     contains(point) { return false; }
     onPointerDown(point) { }
     onPointerMove(point) { }
@@ -20,14 +23,14 @@ class Drawable {
 }
 
 class Robot extends Drawable {
-    width = 50;
-    height = 100;
+    width = 0.2;
+    height = 0.5;
 
     constructor(scene) {
         super(scene, "robot");
-        this.position = { x: 100.0, y: 100.0 };
+        this.position = { x: 0.0, y: 0.0 };
         this.angle = 90.0;
-        this.rotation_handle = new Point(scene, this, this.handle_position(), 5);
+        this.rotation_handle = new Point(scene, this, this.handle_position(), 7);
     }
 
     children() {
@@ -37,7 +40,7 @@ class Robot extends Drawable {
     handle_position() {
         return {
             x: this.position.x + Math.cos(this.angle * Math.PI / 180.0) * this.height / 2,
-            y: this.position.y - Math.sin(this.angle * Math.PI / 180.0) * this.height / 2,
+            y: this.position.y + Math.sin(this.angle * Math.PI / 180.0) * this.height / 2,
         }
     }
 
@@ -89,20 +92,20 @@ class Robot extends Drawable {
 
     onChildMove(point, child) {
         let x = point.x - this.position.x;
-        let y = -(point.y - this.position.y);
+        let y = point.y - this.position.y;
 
         this.angle = Math.atan2(y, x) * 180.0 / Math.PI;
         this.angle = (this.angle + 360.0) % 360.0;
-        let margin = 5.0;
+        let margin = 5.0; // degrees
         function nearly_equal(a, b) {
             let x = Math.abs(a - b);
             return x < margin || (360.0 - x) < margin;
         }
 
-        let snap_points = [0.0, 45.0, 90.0, 135.0, 180.0, 225.0, 270.0, 315.0];
-        for (var i = 0; i < snap_points.length; i++) {
-            if (nearly_equal(snap_points[i], this.angle)) {
-                this.angle = snap_points[i];
+        let snap_angles = [0.0, 45.0, 90.0, 135.0, 180.0, 225.0, 270.0, 315.0];
+        for (var i = 0; i < snap_angles.length; i++) {
+            if (nearly_equal(snap_angles[i], this.angle)) {
+                this.angle = snap_angles[i];
                 break;
             }
         }
@@ -110,14 +113,13 @@ class Robot extends Drawable {
         this.rotation_handle.update_position(this.handle_position());
     }
 
-    draw() {
-        let ctx = canvas.getContext("2d");
-        ctx.lineWidth = 3;
+    draw(ctx) {
+        ctx.lineWidth = this.scene.scale(5);
         ctx.strokeStyle = "black";
 
         ctx.save();
         ctx.translate(this.position.x, this.position.y);
-        ctx.rotate(-this.angle*Math.PI/180.0);
+        ctx.rotate(this.angle * Math.PI / 180.0);
 
         ctx.beginPath();
         ctx.rect(-this.height / 2, -this.width / 2, this.height, this.width);
@@ -223,9 +225,8 @@ class Polygon extends Drawable {
         }
     }
 
-    draw() {
-        let ctx = canvas.getContext("2d");
-        ctx.lineWidth = 3;
+    draw(ctx) {
+        ctx.lineWidth = this.scene.scale(3);
         ctx.fillStyle = "black";
         ctx.strokeStyle = this.closed && this.scene.active_object != this ? "green" : "blue";
 
@@ -238,6 +239,7 @@ class Polygon extends Drawable {
         if (this.closed) {
             ctx.closePath();
         }
+
         ctx.fill();
         ctx.stroke();
     }
@@ -263,7 +265,7 @@ class Point extends Drawable {
         if (this.scene.active_object != null) { return false; }
         let distance = Math.sqrt((point.x - this.point.x) ** 2 + (point.y - this.point.y) ** 2);
 
-        return distance <= 3 * this.size;
+        return distance <= 3 * this.scene.scale(this.size);
     }
 
     onPointerDown(point) {
@@ -284,25 +286,24 @@ class Point extends Drawable {
         this.scene.apply(action);
     }
 
-    draw() {
-        let ctx = canvas.getContext('2d');
+    draw(ctx) {
         ctx.fillStyle = "blue";
 
         ctx.beginPath();
-        ctx.arc(this.point.x, this.point.y, this.size, 0, 2 * Math.PI, false);
+        ctx.arc(this.point.x, this.point.y, this.scene.scale(this.size), 0, 2 * Math.PI, false);
         ctx.closePath();
         ctx.fill();
     }
 }
 
 class Waypoints extends Drawable {
-    waypoint_size = 8;
+    waypoint_size = 5;
 
     constructor(scene) {
         super(scene, "waypoints");
         this.waypoints = [
-            new Point(scene, this, { x: 200, y: 200 }, this.waypoint_size),
-            new Point(scene, this, { x: 200, y: 500 }, this.waypoint_size),
+            new Point(scene, this, { x: 1.0, y: -1.0 }, this.waypoint_size),
+            new Point(scene, this, { x: 1.0, y: 1.0 }, this.waypoint_size),
         ];
     }
 
@@ -337,7 +338,7 @@ class Waypoints extends Drawable {
 
     contains(point) {
         let closest = this.closest_segment(point);
-        return closest[1] < 3*this.waypoint_size;
+        return closest[1] < 3*this.scene.scale(this.waypoint_size);
     }
 
     onPointerDown(point) {
@@ -348,9 +349,8 @@ class Waypoints extends Drawable {
         }
     }
 
-    draw() {
-        let ctx = canvas.getContext("2d");
-        ctx.lineWidth = 3;
+    draw(ctx) {
+        ctx.lineWidth = this.scene.scale(5);
         ctx.strokeStyle = "purple";
 
         ctx.beginPath();
@@ -422,7 +422,7 @@ class ExtendPolygon extends Action {
 
         let start = this.polygon.vertices[0].point;
         let distance_to_start = Math.sqrt((start.x - this.vertex.point.x) ** 2 + (start.y - this.vertex.point.y) ** 2);
-        if (distance_to_start < 3 * this.polygon.vertex_size) {
+        if (distance_to_start < 3 * this.scene.scale(this.polygon.vertex_size)) {
             this.polygon.closed = true;
             this.scene.set_active(null);
         } else {
@@ -498,7 +498,7 @@ class Scene {
     }
 
     draw_object(ctx, object) {
-        object.draw();
+        object.draw(ctx);
         let children = object.children();
 
         for (var i = 0; i < children.length; i++) {
@@ -506,12 +506,54 @@ class Scene {
         }
     }
 
+    clear_background(ctx) {
+        ctx.save();
+        ctx.setTransform(1, 0, 0, 1, 0, 0);
+        ctx.fillStyle = '#ddd';
+        ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+        ctx.restore();
+    }
+
+    // transform canvas to have origin in center, X & Y like standard Cartesian coordinates,
+    // and scaled to fit map to canvas
+    transform_canvas(ctx) {
+        let canvas_size = Math.min(canvas.width, canvas.height);
+        let map_size = config.map_size;
+        let scale = canvas_size / map_size;
+
+        ctx.resetTransform();
+        ctx.translate(canvas.width / 2.0, canvas.height / 2.0);
+        ctx.scale(scale, -scale);
+    }
+
+    // transform mouse/touch pointer coordinates to match transformed canvas
+    get_pointer(event) {
+        let x = parseInt(event.clientX - canvas.offsetLeft);
+        let y = parseInt(event.clientY - canvas.offsetTop);
+
+        let canvas_size = Math.min(canvas.width, canvas.height);
+        let map_size = config.map_size;
+        let scale = map_size / canvas_size;
+
+        x = (x - (canvas.width / 2.0)) * scale;
+        y = (y - (canvas.height / 2.0)) * (-scale);
+
+        return { x, y };
+    }
+
+    scale(size) {
+        let canvas_size = Math.min(canvas.width, canvas.height);
+        let map_size = config.map_size;
+        let scale = map_size / canvas_size;
+
+        return size * scale;
+    }
+
     draw() {
         let ctx = canvas.getContext('2d');
+        this.clear_background(ctx);
 
-        // clear canvas
-        ctx.fillStyle = '#ddd';
-        ctx.fillRect(0, 0, canvas.width, canvas.height);
+        this.transform_canvas(ctx);
 
         for (var i = 0; i < this.objects.length; i++) {
             this.draw_object(ctx, this.objects[i]);
@@ -616,10 +658,7 @@ function waypoint_mode() {
 }
 
 function onPointerDown(event) {
-    let pointer = {
-        x: parseInt(event.clientX - canvas.offsetLeft),
-        y: parseInt(event.clientY - canvas.offsetTop),
-    };
+    let pointer = scene.get_pointer(event);
 
     let target = scene.find_collision(pointer);
     if (target != null) {
@@ -637,7 +676,7 @@ function onPointerDown(event) {
                 scene.apply(action);
             }
         } else if (drawing_mode == Modes.Waypoint) {
-            let action = new ExtendWaypoints(scene, waypoints, waypoints.waypoints.length+1, pointer);
+            let action = new ExtendWaypoints(scene, waypoints, waypoints.waypoints.length, pointer);
             scene.apply(action);
         }
     }
@@ -646,10 +685,7 @@ function onPointerDown(event) {
 }
 
 function onPointerMove(event) {
-    let pointer = {
-        x: parseInt(event.clientX - canvas.offsetLeft),
-        y: parseInt(event.clientY - canvas.offsetTop),
-    };
+    let pointer = scene.get_pointer(event);
 
     if (scene.active_object != null) {
         scene.active_object.onPointerMove(pointer);
@@ -659,10 +695,7 @@ function onPointerMove(event) {
 }
 
 function onPointerUp(event) {
-    let pointer = {
-        x: parseInt(event.clientX - canvas.offsetLeft),
-        y: parseInt(event.clientY - canvas.offsetTop),
-    };
+    let pointer = scene.get_pointer(event);
 
     if (scene.active_object != null) {
         scene.active_object.onPointerUp(pointer);
